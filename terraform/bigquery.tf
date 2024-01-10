324a97b75308feb2c4b13209f33a464b4021b24b
Will Brown <brown5628@gmail.com>
2024-01-09 20:24:07 -0500

    add google drive

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -16,3 +16,22 @@ resource "google_bigquery_dataset" "gcs_raw_dataset" {
     user_by_email = google_service_account.bq_owner.email
   }
 }
+
+resource "google_bigquery_dataset" "google_drive_raw_dataset" {
+  project                     = var.project_id
+  dataset_id                  = "google_drive_raw"
+  friendly_name               = "google_drive_raw"
+  description                 = "External tables for Google Drive."
+  location                    = var.region
+  default_table_expiration_ms = 3600000
+
+  access {
+    role          = "WRITER"
+    user_by_email = google_service_account.dbt_sa.email
+  }
+
+  access {
+    role          = "OWNER"
+    user_by_email = google_service_account.bq_owner.email
+  }
+}
